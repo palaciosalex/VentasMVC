9047d9c1c6c9902f92ceb335bf3fab921097a3b2
Alex palacios <davixdmarzo97@gmail.com>
2020-08-03 12:12:24 -0500

    Creacion clase producto

--- src/java/Modelo/Producto.java
+++ src/java/Modelo/Producto.java
@@ -10,5 +10,62 @@ package Modelo;
  * @author Alex
  */
 public class Producto {
+    private int idproducto;
+    private String descripcion;
+    private float precio;
+    private int stock;
+    private String estado;
+
+    public Producto() {
+    }
+
+    public Producto(int idproducto, String descripcion, float precio, int stock, String estado) {
+        this.idproducto = idproducto;
+        this.descripcion = descripcion;
+        this.precio = precio;
+        this.stock = stock;
+        this.estado = estado;
+    }
+
+    public int getIdproducto() {
+        return idproducto;
+    }
+
+    public void setIdproducto(int idproducto) {
+        this.idproducto = idproducto;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public float getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(float precio) {
+        this.precio = precio;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+    
     
 }
